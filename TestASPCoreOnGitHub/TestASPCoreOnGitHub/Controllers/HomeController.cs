f0fde2798fea237c9a3b6697c142b4e66df4a3ba
ibrahim Helmy <ibrahimhelmy2014@hotmail.com>
2020-09-14 14:11:56 +0200

    Add MVC to application

---
+++ TestASPCoreOnGitHub/TestASPCoreOnGitHub/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TestASPCoreOnGitHub.Controllers
+{
+    public class HomeController : Controller   // to make it controller you should inherit from Controller Class in  MVC name space
+    {
+        public string Index()
+        {
+            return "Hello from MVC contoroller";
+        }
+
+
+    }
+}
--- TestASPCoreOnGitHub/TestASPCoreOnGitHub/Startup.cs
+++ TestASPCoreOnGitHub/TestASPCoreOnGitHub/Startup.cs
@@ -16,6 +16,10 @@ namespace TestASPCoreOnGitHub
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            //services.AddMvc(); /// to add mvc application patern 
+            services.AddControllersWithViews(); // to add controller and views 
+            // services.AddRazorPages();  // to add htmlcss files (view)
+           // services.AddControllers();  /// to add webapi use addcontrollers pattern only
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -26,15 +30,105 @@ namespace TestASPCoreOnGitHub
                 app.UseDeveloperExceptionPage();
             }
 
+
+           // to add new custome middle ware
+            //
+            app.Use(async (context, next) =>
+            {
+
+                await context.Response.WriteAsync("Env. Name :" + env.EnvironmentName + "\n");
+
+                await context.Response.WriteAsync("Hello from my first Middle ware (M1)" + "\n");
+                // to go to next middle ware we should call next method here
+                await next();
+
+
+                await context.Response.WriteAsync("Hello from my first Middle ware (M1) response " + "\n");
+
+            });
+
+
+
+
+
+            // by this code it will not going to next middle ware cause we did not call next method
+            app.Use(async (context, next) =>
+            {
+
+                await context.Response.WriteAsync("Hello from my seconed Middle ware (M2)" + "\n");
+                //to go to next middle ware we should call next method here
+                await next();
+
+                await context.Response.WriteAsync("Hello from my seconed Middle ware (M2) response " + "\n");
+            });
+
+
+            app.Use(async (context, next) =>
+            {
+
+                await context.Response.WriteAsync("Hello from my Third Middle ware (M3)" + "\n");
+                await next();
+
+
+            });
+
+
+
+
+            // so before using endpoint you should userouting or will throw this exception
+            /// Please add EndpointRoutingMiddleware by calling 'IApplicationBuilder.UseRouting
+
+            /// the order of using middle ware here is matter it mean if you call endpoint before routing will give you an excption too
             app.UseRouting();
 
+            // map url (your route ) to particular resource 
             app.UseEndpoints(endpoints =>
             {
+
+                /// here the maping 
+                /// the defualt  url is "/" means the domain
+                /// 
+                ///// mapget will handle only the get request 
+                /// map method will handel all requests 
                 endpoints.MapGet("/", async context =>
                 {
-                    await context.Response.WriteAsync("Hello World!");
+
+                    if (env.IsDevelopment())
+                    {
+                        await context.Response.WriteAsync("Hello from Dev. Env. \n");
+                    }
+                    else if(env.IsProduction())
+                    {
+                        await context.Response.WriteAsync("Hello from  Prod. Env. \n");
+                    }
+                    else if (env.IsStaging())
+                    {
+                        await context.Response.WriteAsync("Hello from  Stag. Env. \n");
+                    }
+                    else 
+                    {
+                        await context.Response.WriteAsync("Hello from Custome Env. \n");
+                    }
+
+
                 });
+
+
+                endpoints.MapGet("/Ibrahim", async context =>
+                {
+                    await context.Response.WriteAsync("Hello Ibrahim!" + "\n");
+                });
+
+
+
+                endpoints.MapControllerRoute(name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}" );
+
+
             });
+
+
+          
         }
     }
 }
